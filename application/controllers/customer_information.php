f7ba5da154ce534965be27c809788a3a43c2e6e9
Erick <erickmasanque@gmail.com>
2012-10-20 23:53:14 +0800

    Pictures and Customer Information FOrm
    Added Customer Information Form(Incomplete)
    Deleted the pictures (too much space)

---
+++ application/controllers/customer_information.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Customer_information extends CI_Controller {
+
+	public function index()
+	{
+		echo "this is customer information";
+		$this->load->helper('html');
+		$this->load->view('welcome_head');
+		$this->load->view('/customer_information/customer_information_body');
+		
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
+?>
---
+++ application/views/customer_information/customer_information_body.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Customer Information</title>
+	<style type="text/css">
+
+	
+	</style>
+</head>
+<body>
+
+<div id="container">
+	
+	<div id="body">
+		<p>Customer Information Body</div>
+		<form>
+			First name: <input type="text" name="firstname">
+			Last name: <input type="text" name="lastname">
+			Address: <input type="text" name="address">
+			Contact Number: <input type="text" name="contact_number">
+		</form>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
