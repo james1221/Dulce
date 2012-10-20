de001bf9fda913d5fbefaafe599cf2507d35b25b
Erick Masanque <erickmasanque@gmail.com>
2012-10-20 16:56:54 +0800

    Created the homepage views
    created 3 homepage view
    edited the controller "welcome" page

---
+++ application/views/welcome_head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Welcome to CodeIgniter</title>
+	<style type="text/css">
+
+	
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Head</h1>
+
+	<div id="body">
+		<p>Body Head	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
