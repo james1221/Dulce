23c809073681efa6db54f8a946171895aea486a9
jameson <james.sedano1286@yahoo.com>
2012-10-21 04:22:44 +0800

    body
    addded body (homepage)

---
+++ application/controllers/test.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		
+		$this->load->helper('html');
+		$this->load->view('test');
+	
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
+?>
--- application/views/welcome_body.php
+++ application/views/welcome_body.php
@@ -1,37 +1,66 @@
 		<div id="main">
             <div id="imageSlidesHolder">
                 <img alt="dulce banner" />
+                
             </div>
             <div id="pageContents">
                 <div class="contentColumn" id="leftColumn">
                         <form method="post">
-                            Login<br />
-                            <input type="text" name="loginName" /><br />
-                            Password<br />
-                            <input type="password" name="loginPassword" /><br />                  		
-                            <input type="submit" value="Login" name="loginSubmit"/>
-                            <input type="button" value="Sign up" name="loginSignUp"/><br />
+                                       		
+                          
+                           
                         </form>
+                        
+ 	<script src="http://code.jquery.com/jquery-latest.js "></script>
+	  <script type="text/javascript" src="http://jzaefferer.github.com/jquery-validation/jquery.validate.js "></script>
+	<style type="text/css">
+	* { font-family: Verdana; font-size: 96%; }
+	label { width: 10em; float: left; }
+	label.error { float: none; color: red; padding-left: .5em; vertical-align: top; }
+	p { clear: both; }
+	.submit { margin-left: 12em; }
+	em { font-weight: bold; padding-right: 1em; vertical-align: top; }
+	</style>
+	  <script>
+	  $(document).ready(function(){
+	    $("#commentForm").validate();
+	  });
+	  </script>
+	  <form class="cmxform" id="commentForm" method="get" action="">
+ <fieldset>                       
+      <label for="cname">Username</label>
+     <input id="cname" name="cname" size="25" class="required" />
+      
+   </p>
+   <p>
+     <label for="cpassword">Password</label>
+     <input id="cpassowrd" name="password" type="password" size="25"  class="Invalid Password" />
+     <input type="submit" value="Login" class="required"  name="loginSubmit"/>
+     <input type="button" value="Sign up" /> 
+    
+   </p>
+                        
+                        
                 </div>
                 <div class="contentColumn" id="centerColumn">
-                	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus suscipit pulvinar. Duis euismod auctor libero sit amet volutpat. In fringilla, erat nec pharetra eleifend, metus turpis faucibus ligula, ac sollicitudin erat turpis a diam. Proin pretium est blandit augue suscipit eu commodo turpis cursus. Donec rhoncus feugiat mi nec ultricies. Mauris nec arcu nec risus congue porta eu eget tortor. Integer id mi mi.
-
-Suspendisse viverra, urna eget tristique volutpat, arcu ipsum egestas ligula, vitae tristique magna ipsum eu libero. Donec ut gravida libero. Ut at turpis et justo vestibulum tincidunt. Nulla tempor gravida vestibulum. Sed ante arcu, laoreet ac rutrum sed, scelerisque at lectus. Sed ut mauris tortor, in sagittis dui. Sed sed iaculis risus. Duis id augue tortor. Nam tempor posuere diam. Vestibulum nunc risus, iaculis at venenatis id, consequat vel mauris. Vestibulum vel libero ligula. In hac habitasse platea dictumst.
-                </div>
+                <input type="button" value="Button to see Testimonials" />
+                <p> testimonials</p>
+                
+                
+            
+                	
+               </div>
                 <div class="contentColumn" id="rightColumn">
 					<iframe width="280" frameborder="0" height="200" scrolling="no" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=14.629363,120.979889&amp;aq=0&amp;oq=abad+santos&amp;ie=UTF8&amp;t=m&amp;z=14&amp;ll=14.629363,120.979889&amp;output=embed"></iframe>
                 </div>
             </div>     
         </div>
         
-        
-        
-<script type="text/javascript">
-var 
-if 
-{
-	
-}
+       
+       
+
+
+  
 
 
 
